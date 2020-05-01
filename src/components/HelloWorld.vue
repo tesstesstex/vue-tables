2e641eaa8cec1b371cc43c51470a6e811083119c
Tessho Nakamura <tesstesstex@gmail.com>
2020-05-02 07:38:52 +0900

    initial

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,122 +1,59 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
-          target="_blank"
-          rel="noopener"
-          >pwa</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div>
+    {{ vmodel }}
+    <vue-good-table
+      :columns="columns"
+      :rows="rows">
+      <template slot="table-row" slot-scope="props">
+        <span v-if="props.column.field == 'description'">
+          <button type="button" class="btn btn-primary">
+          View Details
+          </button>
+        </span>
+        <span v-else-if="props.column.field == 'sex'">
+          <input type="text" :name="'hoge' + props.column.field" v-model="rows[props.index].sex">
+        </span>
+      </template>
+    </vue-good-table>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String
-  }
+  name: 'my-component',
+  data(){
+    return {
+      columns: [
+        {
+          label: 'Name',
+          field: 'name',
+        },
+        {
+          label: 'Age',
+          field: 'age',
+          type: 'number',
+        },
+        {
+          label: 'Description',
+          field: 'description',
+          html: true,
+        },
+        {
+          label: 'Sex',
+          field: 'sex',
+          html: true,
+        },
+      ],
+      vmodel: "",
+      rows: [
+        { id:1, name:"John", age: 20, description: '', sex: 'man' },
+        { id:2, name:"Jane", age: 24, description: '', sex: 'woman' },
+        { id:3, name:"Bob", age: 23, description: '', sex: 'man' },
+        { id:4, name:"Mike", age: 56, description: '', sex: 'man' },
+        { id:5, name:"Kathy", age: 35, description: '', sex: 'woman' },
+        { id:6, name:"Tess", age: 80, description: '', sex: 'woman' },
+      ],
+    };
+  },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueGoodTablePlugin from "vue-good-table";
+import "vue-good-table/dist/vue-good-table.css";
+
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
+Vue.use(VueGoodTablePlugin);
 
 new Vue({
   router,
